d6bbb61ae6cc33c199b5c256e72d280507ff01cf
Subhavpathak <subhavpathak18@gmail.com>
2026-06-17 12:27:07 +0530

    added query param endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app = FastAPI()
 def load_data():
@@ -18,3 +18,25 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/view/{patient_id}") # path parameter
+def view_patient(patient_id: str=Path(...,description='ID of the patient in the DB',example='P001')):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Patient not found")
+
+# endpoints to sort patients
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description="Field to sort by", example="name"), order: str = Query("asc", description="Order of sorting", example="asc")):
+    valid_fields=['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field. Valid fields are: {', '.join(valid_fields)}")
+
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Invalid order. Valid orders are: asc, desc")
+    
+    data = load_data()
+    sorted_data = sorted(data.values(), key=lambda x:x.get(sort_by, 0), reverse=(order=='desc'))
+    return sorted_data
